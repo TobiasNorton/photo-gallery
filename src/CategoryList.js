ebfebac7b2556cb7cfbfb8c0f17772f5f4cf054e
Tobias Norton <tnorton20@yahoo.com>
2018-12-02 20:59:13 -0500

    Removed commented code

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-// import data from './myJSON.json'
 import data from './myJSON.json'
 class CategoryList extends Component {
   render() {
@@ -29,12 +28,3 @@ class CategoryList extends Component {
 }
 
 export default CategoryList
-
-// **WITHOUT ROUTER**
-// Create a photoList state, and set it to undefined
-// Set each <a> with an onClick that calls a function
-// This function sets the state of the photoList, and depending
-// which one it is, it will return a Photolist component instance.
-// Or maybe it should return *either* a panda list OR a miniatures
-// list? That would make more sense, because it's more like a
-// different page.
